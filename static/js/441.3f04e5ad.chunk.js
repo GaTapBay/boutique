c88f6b2a58a4d49a69c57521369e560e194b1b09
unknown <quanghoan.creator@gmail.com>
2023-06-01 11:19:34 +0700

    Updates

---
+++ static/js/441.3f04e5ad.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_app=self.webpackChunkreact_app||[]).push([[441],{4441:function(e,a,n){n.r(a),n.d(a,{default:function(){return c}});var s=n(3197),r=(n(2791),n(184));function c(e){return console.log(e.error),(0,r.jsx)("main",{children:(0,r.jsxs)("div",{className:"container align--center",children:[(0,r.jsx)("div",{className:"gap--50"}),(0,r.jsx)("img",{src:s.Sd+"/images/404.jpg",alt:"Oops!"})]})})}}}]);
+//# sourceMappingURL=441.3f04e5ad.chunk.js.map
